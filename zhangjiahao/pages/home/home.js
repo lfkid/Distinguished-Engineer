5c5891c4607648d74fb813aebedafa56adcff233
ZhangJiahao <2609997207@qq.com>
2017-11-09 19:41:45 +0800

    achieve the limit of up and down

--- zhangjiahao/pages/home/home.js
+++ zhangjiahao/pages/home/home.js
@@ -4,20 +4,24 @@ Page({
   data: {
     array:['a','b','c','d','e','f','g'],
     arrayTemp:['a','b','c','d','e','f','g'],
-    arrayMax:['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    arrayMax:['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+    num:7
    }
-   
    ,
+   
+
   addArray:function(e){
     this.data.arrayTemp.push(this.data.arrayMax[this.data.arrayTemp.length])
     this.setData({
-    array:this.data.arrayTemp
+    array:this.data.arrayTemp,
+    num:this.data.arrayTemp.length
    })
   },
   subArray:function(e){
     this.data.arrayTemp.pop()
     this.setData({
-    array:this.data.arrayTemp
+    array:this.data.arrayTemp,
+    num:this.data.arrayTemp.length
   })
 }
 })
