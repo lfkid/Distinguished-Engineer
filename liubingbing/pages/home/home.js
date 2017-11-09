a2fd765217c951699188b2cc1e552d78f6ccff85
LOGIC <15926396569@163.com>
2017-11-09 13:54:28 +0800

    init the button again

--- liubingbing/pages/home/home.js
+++ liubingbing/pages/home/home.js
@@ -1,6 +1,8 @@
 // pages/home/home.js
 Page({
   data: {
+    defaultSize: 'mini',
+    flag: false,
     arr1: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
     arr2: ['h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
   },
