7b015d59bc77ff9c1adab87fb3e47eebc8fcb591
xuyang <872531032@qq.com>
2017-11-09 13:44:08 +0800

    finish botton but have no functio

--- wangxuyang/pages/home/home.js
+++ wangxuyang/pages/home/home.js
@@ -1,6 +1,11 @@
 Page({
-    data: {
-      array: ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-    }
-  })
-  
+  data: {
+    array: ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+  },
+  add: function (e) {
+    console.log("单击");
+  },
+  minus: function (e) {
+    console.log("单击");
+  }
+})
