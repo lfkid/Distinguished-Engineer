3828b5e76bc6923f51c62da5326b19b8ae3d5b2d
ZhangJiahao <2609997207@qq.com>
2017-11-09 18:00:23 +0800

    完成增加减少功能

--- zhangjiahao/pages/home/home.js
+++ zhangjiahao/pages/home/home.js
@@ -2,20 +2,24 @@
 
 Page({
   data: {
-    array:[{mes : 'a'},
-            {mes : 'b'},
-            {mes : 'c'},
-            {mes : 'd'},
-            {mes : 'e'},
-            {mes : 'f'},
-            {mes : 'g'}]
-   },
+    array:['a','b','c','d','e','f','g'],
+    arrayTemp:['a','b','c','d','e','f','g'],
+    arrayMax:['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+   }
+   
+   ,
   addArray:function(e){
-  
+    this.data.arrayTemp.push(this.data.arrayMax[this.data.arrayTemp.length])
+    this.setData({
+    array:this.data.arrayTemp
+   })
   },
   subArray:function(e){
-  
-  }
+    this.data.arrayTemp.pop()
+    this.setData({
+    array:this.data.arrayTemp
+  })
+}
 })
 
 
