b5298b176f6f857ba23576f016882766c1fdea5e
hilree <hilree@outlook.com>
2017-11-10 16:48:23 +0800

    add other data and set rpx

--- huangrong/pages/about/about.js
+++ huangrong/pages/about/about.js
@@ -6,7 +6,30 @@ Page({
     title1: '追凶者也',
     introduction1:'一桩山村凶杀案，三个蠢萌逃亡汉',
     performer1: '刘烨/张泽/段播文/王子文',
-    score1: '7.8'
+    score1: '7.8',
 
+    imgLocation2: '../../asserts/images/qyyas.jpg',
+    title2: '七月与安生',
+    introduction2:'娇滴闺蜜似蜜糖，甜到人家心好慌',
+    performer2: '周冬雨/马思纯/李程彬',
+    score2: '8.6',
+
+    imgLocation3: '../../asserts/images/ytxwz.jpg',
+    title3: '樱桃小丸子:来自意大利的少年',
+    introduction3:'小丸子恋美少年，他竟然不是花轮',
+    performer3: '矶野鳕子/屋良有作/一龙斋贞友',
+    score3: '7.8',
+
+    imgLocation4: '../../asserts/images/cndqsjlg.jpg',
+    title4: '从你的全世界路过',
+    introduction4:'那曾经打动你的，爱你岂止一万年',
+    performer4: '邓超/白百何/杨洋/张天爱/岳云鹏',
+    score4: '8.0',
+
+    imgLocation5: '../../asserts/images/dhxy3.jpg',
+    title5: '大话西游3',
+    introduction5:'月光宝盒重开启，爱你岂止一万年',
+    performer5: '韩庚/唐嫣/莫文蔚/钟欣潼/吴京',
+    score5: '7.6'
   }
 })
