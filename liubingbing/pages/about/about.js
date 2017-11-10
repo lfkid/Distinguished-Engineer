3ee0098adb4705df3ab8d539518dd6f5537665d6
LOGIC <15926396569@163.com>
2017-11-10 18:04:06 +0800

    get data from .js not write data in .wxml

--- liubingbing/pages/about/about.js
+++ liubingbing/pages/about/about.js
@@ -1,6 +1,12 @@
-// pages/about/about.js
+// pages/about/about.js樱桃小丸子: 来自意大利的少年
 Page({
   data: {
-    src: '../../asserts/images/home/cat.jpg'
+    src: '../../asserts/images/home/cat.jpg',
+    firstMovieTitle: "樱桃小丸子: 来自意大利的少年",
+    firstMovieMsg: "小丸子恋美少年，他竟然不是花轮..",
+    firstMovieAuth: "机也学子/屋良有作/一龙斋贞友",
+    // movieTitleList: ["樱桃小丸子: 来自意大利的少年"],
+    // movieMsg: ["小丸子恋美少年，他竟然不是花轮.."],
+    // movieAuthor: ["机也学子/屋良有作/一龙斋贞友"]
   }
 })
