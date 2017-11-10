afb9d6af3030550c526ca730a599a10dc46a0d93
hilree <hilree@outlook.com>
2017-11-10 15:23:55 +0800

    apply css

--- huangrong/pages/about/about.js
+++ huangrong/pages/about/about.js
@@ -1,3 +1,12 @@
 Page({
+  data:{
+    buyDisplay: '购票',
 
+    imgLocation1: '../../asserts/images/zxzy.jpg',
+    title1: '追凶者也',
+    introduction1:'一桩山村凶杀案，三个蠢萌逃亡汉',
+    performer1: '刘烨/张泽/段播文/王子文',
+    score1: '7.8'
+
+  }
 })
