090ce51a14c986c8798e20141f8764b7b41fecb0
lujinyi <15071437004@139.com>
2017-11-13 23:12:44 +0800

    change background color and let data url global

--- lujinyi/pages/about/about.js
+++ lujinyi/pages/about/about.js
@@ -1,11 +1,13 @@
 // pages/about/about.js
+const app = getApp()
+
 Page({
   data:{
     mvMessage:[]
   },
   onLoad:function(){
     wx.request({
-      url:'http://localhost:3000/mvMessage',
+      url:app.globalData.mvUrl,
       header:{
         'Content-Type':'application/json'
       },
