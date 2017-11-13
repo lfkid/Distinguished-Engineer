ce76c2cfe378d0a37ca4f83f2af52bd91217b809
lujinyi <15071437004@139.com>
2017-11-13 13:35:01 +0800

    change movie message to js file

--- lujinyi/pages/about/about.js
+++ lujinyi/pages/about/about.js
@@ -1,9 +1,23 @@
 // pages/about/about.js
 Page({
-  data: {
-    src: [
-      { src1: '../../res/images/nanaqi.jpg' },
-      { src2: '../../res/images/magic.jpg' }
+  data:{
+    mvMessage:[
+      {
+        id:1,
+        src:"../../res/images/nanaqi.jpg",
+        text1:"来自深渊：娜娜奇篇",
+        text2:"深渊前进路上的日常",
+        text3:"娜娜奇/黎明卿",
+        score:8.7
+      },
+      {
+        id:2,
+        src:"../../res/images/magic.jpg",
+        text1:"魔法游行",
+        text2:"小魔女学院前传",
+        text3:"亚可/戴安娜",
+        score:8.9
+      }
     ]
   }
 })
