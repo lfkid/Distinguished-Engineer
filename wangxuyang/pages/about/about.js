3582bb36114592000b1dbf2ecdaca4ea27feb759
xuyang <872531032@qq.com>
2017-11-13 16:31:53 +0800

    use wx:for to add more movies

--- wangxuyang/pages/about/about.js
+++ wangxuyang/pages/about/about.js
@@ -1,5 +1,29 @@
 Page({
   data: {
-    src: '../../asserts/images/a.jpg'
+    pic: [{
+        id: 0,
+        src: '../../asserts/images/a.jpg',
+        title: '愤怒的小鸟1',
+        desc: '爆笑来袭，总有猪在偷笑1',
+        dubbing: '杰森 / 乔什 / 丹尼',
+        score: '8.0分'
+      },
+      {
+        id: 1,
+        src: '../../asserts/images/b.jpg',
+        title: '愤怒的小鸟2',
+        desc: '爆笑来袭，总有猪在偷笑2',
+        dubbing: '琼恩 / 乔什 / 丹尼',
+        score: '8.5分'
+      },
+      {
+        id: 2,
+        src: '../../asserts/images/c.jpg',
+        title: '愤怒的小鸟3',
+        desc: '爆笑来袭，总有猪在偷笑3',
+        dubbing: '杰森 / 乔什 / 丹尼',
+        score: '9.0分'
+      },
+    ]
   }
 })
