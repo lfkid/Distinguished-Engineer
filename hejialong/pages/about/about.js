fcbe26f13c0f9746baad7d92bfa84d1de7dbf7db
dragonhaw <dragonhaw@163.com>
2017-11-13 20:52:32 +0800

    update about pages

--- hejialong/pages/about/about.js
+++ hejialong/pages/about/about.js
@@ -1,30 +1,22 @@
+const app = getApp();
+const movieApi = app.globalData.url;
+
 Page({
   data: {
-    movie: [
-      {
-        imgPath: "../../asserts/images/东方快车谋杀案.png",
-        title: '东方快车谋杀案',
-        subTitle: 'Murder on the Orient Express',
-        actor: '韩庚 / 唐嫣 / 莫文蔚 / 钟欣潼',
-        score: '8.3分',
-        action: '购票'
-      },
-      {
-        imgPath: "../../asserts/images/雷神3：诸神黄昏.png",
-        title: '雷神3：诸神黄昏',
-        subTitle: 'Thor: Ragnarök',
-        actor: '韩庚 / 唐嫣 / 莫文蔚 / 钟欣潼',
-        score: '8.9分',
-        action: '购票'
-      },
-      {
-        imgPath: "../../asserts/images/狂兽.png",
-        title: '狂兽',
-        subTitle: 'The Brink',
-        actor: '韩庚 / 唐嫣 / 莫文蔚 / 钟欣潼',
-        score: '7.6分',
-        action: '购票'
-      }
-    ],
+    movie: [],
   },
+
+  onLoad: function () {
+    wx.request({
+      url: movieApi,
+      success: (res) => {
+        let list = res.data;
+        if (Array.isArray(list) && list.length > 0) {
+          this.setData({
+            movie: list
+          });
+        }
+      }
+    })
+  }
 })
