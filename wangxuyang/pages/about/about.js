d19e5e4e0ed2e21c0bd86b1a6539bc02c3db4940
xuyang <872531032@qq.com>
2017-11-13 18:08:16 +0800

    finish request from web

--- wangxuyang/pages/about/about.js
+++ wangxuyang/pages/about/about.js
@@ -1,29 +1,17 @@
 Page({
   data: {
-    pic: [{
-        id: 0,
-        src: '../../asserts/images/a.jpg',
-        title: '愤怒的小鸟1',
-        desc: '爆笑来袭，总有猪在偷笑1',
-        dubbing: '杰森 / 乔什 / 丹尼',
-        score: '8.0分'
-      },
-      {
-        id: 1,
-        src: '../../asserts/images/b.jpg',
-        title: '愤怒的小鸟2',
-        desc: '爆笑来袭，总有猪在偷笑2',
-        dubbing: '琼恩 / 乔什 / 丹尼',
-        score: '8.5分'
-      },
-      {
-        id: 2,
-        src: '../../asserts/images/c.jpg',
-        title: '愤怒的小鸟3',
-        desc: '爆笑来袭，总有猪在偷笑3',
-        dubbing: '杰森 / 乔什 / 丹尼',
-        score: '9.0分'
-      },
-    ]
+    movie: []
+  },
+  onLoad: function () {
+    var that = this
+    wx.request({
+      url: 'http://localhost:3000/posts',
+      success: (res) => {
+        // console.log(res.data)
+        that.setData({
+          movie: res.data
+        })
+      }
+    })
   }
 })
