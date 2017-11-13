a59b7778a3b9f5f206958474a56fb84656368114
ZhangJiahao <2609997207@qq.com>
2017-11-13 14:07:22 +0800

    using template

--- zhangjiahao/pages/about/about.js
+++ zhangjiahao/pages/about/about.js
@@ -1,4 +1,30 @@
 // pages/about/about.js
 Page({
+data:{
 
+  movie:[
+
+    {
+      imgPath: "/asserts/images/movie/world.jfif",
+      name: '从你的全世界路过',
+      introduce: '那曾打动你的，还会继续温暖你',
+      actor: '邓超/白百合/杨洋/张天爱/云岳鹏',
+      score: '8.3分',
+    },
+    {
+      imgPath: "/asserts/images/movie/world.jfif",
+      name: '从你的全世界路过',
+      introduce: '那曾打动你的，还会继续温暖你',
+      actor: '邓超/白百合/杨洋/张天爱/云岳鹏',
+      score: '8.3分',
+    },
+    {
+      imgPath: "/asserts/images/movie/world.jfif",
+      name: '从你的全世界路过',
+      introduce: '那曾打动你的，还会继续温暖你',
+      actor: '邓超/白百合/杨洋/张天爱/云岳鹏',
+      score: '8.3分',
+    },
+        ]
+},
 })
