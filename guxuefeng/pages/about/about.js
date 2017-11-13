90c61bbb36ac8bbc322e8e13a011a5b93dde8340
guxuefeng <gxf41x@163.com>
2017-11-13 23:11:58 +0800

    added two functional buttons in fixed position

--- guxuefeng/pages/about/about.js
+++ guxuefeng/pages/about/about.js
@@ -1,1 +1,87 @@
-Page({})
+Page({
+  data: {
+    array1: [{
+      id:1,
+      path : "../../assert/image/film1.jpg",
+      title: "七月与安生",
+      message: "娇滴闺蜜似蜜糖，舔到人家心好慌",
+      actor: "周冬雨 / 马思纯 / 李程彬",
+      score: "8.6分",
+      buy: "购买"
+    },{
+    id:2,
+    path : "../../assert/image/film1.jpg",
+    title: "七月与安生",
+    message: "娇滴闺蜜似蜜糖，舔到人家心好慌",
+    actor: "周冬雨 / 马思纯 / 李程彬",
+    score: "8.6分",
+    buy: "购买"
+  },
+  {
+    id:3,
+    path : "../../assert/image/film1.jpg",
+    title: "七月与安生",
+    message: "娇滴闺蜜似蜜糖，舔到人家心好慌",
+    actor: "周冬雨 / 马思纯 / 李程彬",
+    score: "8.6分",
+    buy: "购买"
+  },
+  {
+    id:4,
+    path : "../../assert/image/film1.jpg",
+    title: "七月与安生",
+    message: "娇滴闺蜜似蜜糖，舔到人家心好慌",
+    actor: "周冬雨 / 马思纯 / 李程彬",
+    score: "8.6分",
+    buy: "购买"
+  },{
+    id:5,
+    path : "../../assert/image/film1.jpg",
+    title: "七月与安生",
+    message: "娇滴闺蜜似蜜糖，舔到人家心好慌",
+    actor: "周冬雨 / 马思纯 / 李程彬",
+    score: "8.6分",
+    buy: "购买"
+  },{
+    id:6,
+    path : "../../assert/image/film1.jpg",
+    title: "七月与安生",
+    message: "娇滴闺蜜似蜜糖，舔到人家心好慌",
+    actor: "周冬雨 / 马思纯 / 李程彬",
+    score: "8.6分",
+    buy: "购买"
+  },{
+    id:7,
+    path : "../../assert/image/film1.jpg",
+    title: "七月与安生",
+    message: "娇滴闺蜜似蜜糖，舔到人家心好慌",
+    actor: "周冬雨 / 马思纯 / 李程彬",
+    score: "8.6分",
+    buy: "购买"
+  },{
+    id:8,
+    path : "../../assert/image/film1.jpg",
+    title: "七月与安生",
+    message: "娇滴闺蜜似蜜糖，舔到人家心好慌",
+    actor: "周冬雨 / 马思纯 / 李程彬",
+    score: "8.6分",
+    buy: "购买"
+  },{
+    id:9,
+    path : "../../assert/image/film1.jpg",
+    title: "七月与安生",
+    message: "娇滴闺蜜似蜜糖，舔到人家心好慌",
+    actor: "周冬雨 / 马思纯 / 李程彬",
+    score: "8.6分",
+    buy: "购买"
+  },{
+    id:10,
+    path : "../../assert/image/film1.jpg",
+    title: "七月与安生",
+    message: "娇滴闺蜜似蜜糖，舔到人家心好慌",
+    actor: "周冬雨 / 马思纯 / 李程彬",
+    score: "8.6分",
+    buy: "购买"
+  }]
+  }
+})
