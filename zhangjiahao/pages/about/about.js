2752fbde819f9426898590ea453be493535e3bd6
ZhangJiahao <2609997207@qq.com>
2017-11-14 15:14:18 +0800

    add the headLine tousing property hidden

--- zhangjiahao/pages/about/about.js
+++ zhangjiahao/pages/about/about.js
@@ -1,31 +1,27 @@
 // pages/about/about.js
+var that;
 Page({
-data:{
+  data: {
 
-  movie:[
+    movie: []
 
-    {
-      imgPath: "/asserts/images/movie/world.jfif",
-      name: '从你的全世界路过',
-      introduce: '那曾打动你的，还会继续温暖你',
-      actor: '邓超/白百合/杨洋/张天爱/云岳鹏',
-      score: '8.3分',
-    },
-    {
-      imgPath: "/asserts/images/movie/world.jfif",
-      name: '从你的全世界路过',
-      introduce: '那曾打动你的，还会继续温暖你',
-      actor: '邓超/白百合/杨洋/张天爱/云岳鹏',
-      score: '8.3分',
-    },
-    {
-      imgPath: "/asserts/images/movie/world.jfif",
-      name: '从你的全世界路过',
-      introduce: '那曾打动你的，还会继续温暖你',
-      actor: '邓超/白百合/杨洋/张天爱/云岳鹏',
-      score: '8.3分',
-    },
-        ]
-
-},
+  },
+  onLoad: function () {
+    that = this;
+    wx.request({
+      url: 'http://localhost:3000/customers',
+      success: function (res) {
+        console.log(res.data);
+        that.setData({
+          movie: res.data,
+        })
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  }
 })
