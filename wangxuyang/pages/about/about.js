93e50ee15ccfd6a132ed4f5a68a726df9d3434c9
xuyang <872531032@qq.com>
2017-11-15 17:59:35 +0800

    finish hidden function

--- wangxuyang/pages/about/about.js
+++ wangxuyang/pages/about/about.js
@@ -1,9 +1,23 @@
 Page({
   data: {
     movie: [],
+    movie1: [],
     cart: '购物车',
     balance: '结算',
-    message: '1'
+    message: '1',
+    hoting: '热映',
+    waitting: '待映',
+    tap: true
+  },
+  tapLeft: function(res) {
+    this.setData({
+      tap: true,
+    })
+  },
+  tapRight: function(res) {
+    this.setData({
+      tap: false,
+    })
   },
   onLoad: function () {
     var that = this
@@ -13,6 +27,14 @@ Page({
         // console.log(res.data)
         that.setData({
           movie: res.data
+        })
+      }
+    })
+    wx.request({
+      url: 'http://localhost:3000/movie1',
+      success: (res) => {
+        that.setData({
+          movie1: res.data
         })
       }
     })
